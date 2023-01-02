da5b2b3a2e68c90edeb82d2cbe9c2015abb3a6ea
ziggie <ziggie1984@protonmail.com>
2023-01-02 12:38:56 +0100

    add control of the fee at the last hop

--- mission_ctl.go
+++ mission_ctl.go
@@ -12,6 +12,20 @@ func (r *regolancer) addFailedChan(fromStr string, toStr string, amount int64) {
 }
 
 func (r *regolancer) validateRoute(route *lnrpc.Route) error {
+	targetHop := route.Hops[len(route.Hops)-2]
+	if int64(float64(targetHop.FeeMsat)/float64(targetHop.AmtToForwardMsat)*1e6) > params.FeeLastHopPPM && params.FeeLastHopPPM != 0 {
+		from, err := hex.DecodeString(targetHop.PubKey)
+		if err != nil {
+			return err
+		}
+		to, err := hex.DecodeString(r.myPK)
+		if err != nil {
+			return err
+		}
+		r.failedPairs = append(r.failedPairs, &lnrpc.NodePair{From: from, To: to})
+		return fmt.Errorf("last hop with chan %d exceeds our limit: %s ppm allowed, wanted %s ppm", route.Hops[len(route.Hops)-1].ChanId, hiWhiteColor(params.FeeLastHopPPM), formatFeePPM(targetHop.AmtToForwardMsat, targetHop.FeeMsat))
+	}
+
 	prevHopPK := r.myPK
 	for _, h := range route.Hops {
 		hopPK := h.PubKey
